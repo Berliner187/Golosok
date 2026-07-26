38f490cfa91398f2de278c58d32b37085af2a09f
Rostislav <fadenougat@gmail.com>
2026-07-26 17:25:00 +0300

    Initial Commit

---
+++ Golosok/GolosokApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolosokApp.swift
+//  Golosok
+//
+//  Created by kozak_dev on 26.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct GolosokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GolosokTests/GolosokTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolosokTests.swift
+//  GolosokTests
+//
+//  Created by kozak_dev on 26.07.2026.
+//
+
+import Testing
+@testable import Golosok
+
+struct GolosokTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
